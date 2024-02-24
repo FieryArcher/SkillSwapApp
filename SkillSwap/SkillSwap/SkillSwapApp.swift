abfe75ceb59bf7570eb765a67552a4735024d6d3
Nurlan Darzhanov <ndarzhanov3@gmail.com>
2024-02-24 16:53:32 +0600

    Initial commit
    Creating project in xcode

---
+++ SkillSwap/SkillSwap/SkillSwapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillSwapApp.swift
+//  SkillSwap
+//
+//  Created by Nurlan Darzhanov on 24.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SkillSwapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
